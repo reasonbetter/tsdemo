2adf4fe46a12426e628cfdd97b6cbe9c81f17083
David <dmanley8@gmail.com>
2025-09-12 13:59:47 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -545,9 +545,10 @@ export default function Home() {
                             />
                             {/* Visual confirmation when saved (input value matches the saved userTag) */}
                             {userIdInput === userTag && userTag !== "" && (
-                                <svg className="w-5 h-5 text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" title="ID Saved">
-                                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L9 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                </svg>
+                              <svg className="w-5 h-5 text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+  <title>ID Saved</title>
+  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L9 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+</svg>
                             )}
                         </div>
                     </div>

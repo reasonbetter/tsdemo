bc1ee20d50643367a7d48d38620b279e873a539f
David <dmanley8@gmail.com>
2025-09-18 21:41:21 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
     [currentId]
   );
 
-  // --- helpers, API calls, submit handlers ----------------------------------------------------------------
+  // --- helpers, API calls, submit handlers ---
 
     async function logEvent(type: string, payload: Record<string, any>, specificSessionId?: string): Promise<void> {
         const sid = specificSessionId || sessionId;
@@ -490,7 +490,7 @@ export default function Home() {
                             />
                             {userIdInput === userTag && userTag !== "" && (
                                 <>
-                                    <svg className="w-5 h-5 text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0" 0 20 20" fill="currentColor">
+                                    <svg className="w-5 h-5 text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                         <title>ID Saved</title>
                                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L9 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                     </svg>
